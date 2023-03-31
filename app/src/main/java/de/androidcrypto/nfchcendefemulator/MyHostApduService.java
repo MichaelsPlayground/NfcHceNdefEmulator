d641de1dd9bff0aa8ab6aca3450bb4aff30c5946
michaelfehr <michael_fehrgmx.de>
2023-03-31 13:51:47 +0200

    updated

--- app/src/main/java/de/androidcrypto/nfchcendefemulator/MyHostApduService.java
+++ app/src/main/java/de/androidcrypto/nfchcendefemulator/MyHostApduService.java
@@ -123,14 +123,16 @@ public class MyHostApduService extends HostApduService {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent.hasExtra("ndefMessage")) {
-            NdefMessage ndefMessage = getNdefMessage(intent.getStringExtra("ndefMessage"));
-            if (ndefMessage != null) {
-                int nlen = ndefMessage.getByteArrayLength();
-                mNdefRecordFile = new byte[nlen + 2];
-                mNdefRecordFile[0] = (byte) ((nlen & 0xff00) / 256);
-                mNdefRecordFile[1] = (byte) (nlen & 0xff);
-                System.arraycopy(ndefMessage.toByteArray(), 0, mNdefRecordFile, 2, ndefMessage.getByteArrayLength());
+        if (intent != null) {
+            if (intent.hasExtra("ndefMessage")) {
+                NdefMessage ndefMessage = getNdefMessage(intent.getStringExtra("ndefMessage"));
+                if (ndefMessage != null) {
+                    int nlen = ndefMessage.getByteArrayLength();
+                    mNdefRecordFile = new byte[nlen + 2];
+                    mNdefRecordFile[0] = (byte) ((nlen & 0xff00) / 256);
+                    mNdefRecordFile[1] = (byte) (nlen & 0xff);
+                    System.arraycopy(ndefMessage.toByteArray(), 0, mNdefRecordFile, 2, ndefMessage.getByteArrayLength());
+                }
             }
         }
         return super.onStartCommand(intent, flags, startId);

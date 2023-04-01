c9108a57d27bfbfcce76684307a07c336b07559e
michaelfehr <michael_fehrgmx.de>
2023-04-01 13:46:20 +0200

    updated

--- app/src/main/java/de/androidcrypto/nfchcendefemulator/MyHostApduService.java
+++ app/src/main/java/de/androidcrypto/nfchcendefemulator/MyHostApduService.java
@@ -5,6 +5,7 @@ import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.cardemulation.HostApduService;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.Arrays;
 
@@ -15,7 +16,8 @@ import java.util.Arrays;
 public class MyHostApduService extends HostApduService {
 
     // source: https://github.com/TechBooster/C85-Android-4.4-Sample/blob/master/chapter08/NdefCard/src/com/example/ndefcard/NdefHostApduService.java
-    // status: working with long messages
+    
+    private final static String TAG = "MyHostApduService";
 
     private static final byte[] SELECT_APPLICATION = {
             (byte) 0x00, // CLA	- Class - Class of instruction
@@ -36,32 +38,6 @@ public class MyHostApduService extends HostApduService {
             (byte) 0xe1, (byte) 0x03 // file identifier of the CC file
     };
 
-    private static final byte[] READ_CAPABILITY_CONTAINER = {
-            (byte) 0x00, // CLA	- Class - Class of instruction
-            (byte) 0xb0, // INS	- Instruction - Instruction code
-            (byte) 0x00, // P1	- Parameter 1 - Instruction parameter 1
-            (byte) 0x00, // P2	- Parameter 2 - Instruction parameter 2
-            (byte) 0x0f  // Lc field	- Number of bytes present in the data field of the command
-    };
-
-    // In the scenario that we have done a CC read, the same byte[] match
-    // for ReadBinary would trigger and we don't want that in succession
-    private boolean READ_CAPABILITY_CONTAINER_CHECK = false;
-
-    private static final byte[] READ_CAPABILITY_CONTAINER_RESPONSE = {
-            (byte) 0x00, (byte) 0x0F, // CCLEN length of the CC file
-            (byte) 0x20, // Mapping Version 2.0
-            (byte) 0x00, (byte) 0x3B, // MLe maximum 59 bytes R-APDU data size
-            (byte) 0x00, (byte) 0x34, // MLc maximum 52 bytes C-APDU data size
-            (byte) 0x04, // T field of the NDEF File Control TLV
-            (byte) 0x06, // L field of the NDEF File Control TLV
-            (byte) 0xE1, (byte) 0x04, // File Identifier of NDEF file
-            (byte) 0x00, (byte) 0x32, // Maximum NDEF file size of 50 bytes
-            (byte) 0x00, // Read access without any security
-            (byte) 0x00, // Write access without any security
-            (byte) 0x90, (byte) 0x00 // A_OKAY
-    };
-
     private static final byte[] SELECT_NDEF_FILE = {
             (byte) 0x00, // CLA	- Class - Class of instruction
             (byte) 0xa4, // Instruction byte (INS) for Select command
@@ -69,17 +45,6 @@ public class MyHostApduService extends HostApduService {
             (byte) 0x0c, // Parameter byte (P1), select by identifier
             (byte) 0x02, // Lc field	- Number of bytes present in the data field of the command
             (byte) 0xE1, (byte) 0x04 // file identifier of the NDEF file retrieved from the CC file
-    };
-
-    // Status Word success
-    private final static byte[] SUCCESS_SW = new byte[] {
-            (byte)0x90,
-            (byte)0x00,
-    };
-    // Status Word failure
-    private final static byte[] FAILURE_SW = new byte[] {
-            (byte)0x6a,
-            (byte)0x82,
     };
 
     private final static byte[] CAPABILITY_CONTAINER_FILE = new byte[] {
@@ -92,6 +57,17 @@ public class MyHostApduService extends HostApduService {
             (byte) 0x00, (byte) 0xff, // Maximum NDEF size, do NOT extend this value
             0x00, // NDEF file read access granted
             (byte)0xff, // NDEF File write access denied
+    };
+
+    // Status Word success
+    private final static byte[] SUCCESS_SW = new byte[] {
+            (byte)0x90,
+            (byte)0x00,
+    };
+    // Status Word failure
+    private final static byte[] FAILURE_SW = new byte[] {
+            (byte)0x6a,
+            (byte)0x82,
     };
 
     private byte[] mNdefRecordFile;
@@ -173,29 +149,33 @@ public class MyHostApduService extends HostApduService {
      */
     @Override
     public byte[] processCommandApdu(byte[] commandApdu, Bundle extras) {
+        Log.d((TAG), "commandApdu: " + Utils.bytesToHex(commandApdu)); 
         //if (Arrays.equals(SELECT_APP, commandApdu)) {
         // check if commandApdu qualifies for SELECT_APPLICATION
         if (Arrays.equals(SELECT_APPLICATION, commandApdu)) {
             mAppSelected = true;
             mCcSelected = false;
             mNdefSelected = false;
+            Log.d((TAG), "responseApdu: " + Utils.bytesToHex(SUCCESS_SW));
             return SUCCESS_SW;
             // check if commandApdu qualifies for SELECT_CAPABILITY_CONTAINER
         } else if (mAppSelected && Arrays.equals(SELECT_CAPABILITY_CONTAINER, commandApdu)) {
             mCcSelected = true;
             mNdefSelected = false;
+            Log.d((TAG), "responseApdu: " + Utils.bytesToHex(SUCCESS_SW));
             return SUCCESS_SW;
             // check if commandApdu qualifies for SELECT_NDEF_FILE
         } else if (mAppSelected && Arrays.equals(SELECT_NDEF_FILE, commandApdu)) {
             // NDEF
             mCcSelected = false;
             mNdefSelected = true;
+            Log.d((TAG), "responseApdu: " + Utils.bytesToHex(SUCCESS_SW));
             return SUCCESS_SW;
             // check if commandApdu qualifies for // READ_BINARY
         } else if (commandApdu[0] == (byte)0x00 && commandApdu[1] == (byte)0xb0) {
             // READ_BINARY
             // get the offset an le (length) data
-            System.out.println("** " + Utils.bytesToHex(commandApdu) + " in else if (commandApdu[0] == (byte)0x00 && commandApdu[1] == (byte)0xb0) {");
+            //System.out.println("** " + Utils.bytesToHex(commandApdu) + " in else if (commandApdu[0] == (byte)0x00 && commandApdu[1] == (byte)0xb0) {");
             int offset = (0x00ff & commandApdu[2]) * 256 + (0x00ff & commandApdu[3]);
             int le = 0x00ff & commandApdu[4];
 
@@ -204,20 +184,39 @@ public class MyHostApduService extends HostApduService {
             if (mCcSelected && offset == 0 && le == CAPABILITY_CONTAINER_FILE.length) {
                 System.arraycopy(CAPABILITY_CONTAINER_FILE, offset, responseApdu, 0, le);
                 System.arraycopy(SUCCESS_SW, 0, responseApdu, le, SUCCESS_SW.length);
+                Log.d((TAG), "responseApdu: " + Utils.bytesToHex(responseApdu));
                 return responseApdu;
             } else if (mNdefSelected) {
                 if (offset + le <= mNdefRecordFile.length) {
                     System.arraycopy(mNdefRecordFile, offset, responseApdu, 0, le);
                     System.arraycopy(SUCCESS_SW, 0, responseApdu, le, SUCCESS_SW.length);
+                    Log.d((TAG), "responseApdu: " + Utils.bytesToHex(responseApdu));
                     return responseApdu;
                 }
             }
         }
 
-        // The tag should return a different error for different reasons
-        // this emulation just return the general error message
+        // The tag should return different errors for different reasons
+        // this emulation just returns the general error message
+        Log.d((TAG), "responseApdu: " + Utils.bytesToHex(FAILURE_SW));
         return FAILURE_SW;
     }
+    
+/*
+complete sequence:
+commandApdu: 00a4040007d276000085010100
+responseApdu: 9000
+commandApdu: 00a4000c02e103
+responseApdu: 9000
+commandApdu: 00b000000f
+responseApdu: 000f20003b00340406e10400ff00ff9000
+commandApdu: 00a4000c02e104
+responseApdu: 9000
+commandApdu: 00b0000002
+responseApdu: 002e9000
+commandApdu: 00b000022e
+responseApdu: d1012a55046769746875622e636f6d2f416e64726f696443727970746f3f7461623d7265706f7369746f726965739000    
+ */
 
     /**
      * onDeactivated is called when reading ends
